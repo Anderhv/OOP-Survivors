01bdfcd52718ea2cdbf11fa69a16c6555626d66c
AndersHV <anders.h.vagle@live.no>
2023-07-15 17:28:17 +0200

    fix BulletMaterial

--- Assets/SCripts/PlayerController.cs
+++ Assets/SCripts/PlayerController.cs
@@ -30,19 +30,23 @@ public class PlayerController : MonoBehaviour
         {
             return;
         }
-        // Shoot on left mouse click
-        if (Input.GetMouseButtonDown(0))
-        {
-            Shoot();
-        }
-    }
 
-    private void Shoot()
-    {
-        // Shoot a bullet from the player towards the mouse position
+        // Rotate player towards mouse position
         var mousePosition = Input.mousePosition;
         var mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
         mouseWorldPosition.y = 0.3f;
+        transform.LookAt(mouseWorldPosition);
+        
+        // Shoot on left mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot(mouseWorldPosition);
+        }
+    }
+
+    private void Shoot(Vector3 mouseWorldPosition)
+    {
+        // Shoot a bullet from the player towards the mouse position
         var direction = (mouseWorldPosition - transform.position).normalized;
         var bullet = Instantiate(bulletPrefab, transform.position + direction * 0.5f, Quaternion.identity);
         var bulletBehavior = bullet.GetComponent<BulletBehavior>();
